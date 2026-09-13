3ce34dd520dc163c2922b1406892cf545ad0fce7
HTN225BOT <161234083+HTN225BOT@users.noreply.github.com>
2026-09-13 14:25:57 -0400

    Update firebase.js

--- ia/firebase.js
+++ ia/firebase.js
@@ -1,1 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC4ejbTOO1G3XbwDJ5KYH6-CiYcdQqr68k",
+  authDomain: "seg-unda.firebaseapp.com",
+  projectId: "seg-unda",
+  storageBucket: "seg-unda.firebasestorage.app",
+  messagingSenderId: "1076997262231",
+  appId: "1:1076997262231:web:31b640fa8fd5991e8612d2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
